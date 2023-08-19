b01c1e0e38c827e7359e1289ba1439052c074bcd
Michael Garcia <mdgarcia@princeton.edu>
2023-08-19 14:44:06 -0400

    creating project gallery

---
+++ src/js/index.js
@@ -0,0 +1,154 @@
+import styles from "../css/index.scss";
+import din_condensed from "../assets/fonts/DIN Condensed Bold.ttf";
+import din_condensed2 from "../assets/fonts/DIN Condensed Bold.otf";
+import fatfrank from "../assets/fonts/FatFrank-Regular.otf";
+import fatfrank2 from "../assets/fonts/FatFrank-Regular.ttf";
+
+// import 'bootstrap';
+
+const topics = TOPICS.topics
+const tags = TAGS.tags
+const tagsMap = TAGSMAP.tagsMap
+const projects = PROJECTS.projects
+
+let active_filters = []
+let current_category = ""
+
+window.addEventListener('load', (event) => {
+  document.addEventListener('click', event => {
+    const box = document.querySelector('.searchBarContainer');
+    if (!box.contains(event.target)) {
+      document.querySelector(".searchSuggestions").style.display = 'none';
+    }
+  });
+  document.querySelector("#searchCategories").onchange = () => {
+    document.getElementById('searchBar').value = ""
+    current_category = document.querySelector("#searchCategories").value
+    executeSearch("")
+  }
+
+  document.getElementById('searchBar').onclick = (e) => {
+    document.querySelector(".searchSuggestions").style.display = "flex";
+    let queryString = e.target.value;
+
+    let autocompleteResults = []
+    if(current_category != "" && current_category != "No Domain"){
+      autocompleteResults = tagsMap[current_category].filter(x => 
+        x.toLowerCase().startsWith(queryString.toLowerCase().trim()) ||
+        x.toLowerCase().split(" ").includes(queryString.toLowerCase().trim())
+      )
+    }
+    else{
+      autocompleteResults =  tags.filter(x => 
+        x.toLowerCase().startsWith(queryString.toLowerCase().trim()) ||
+        x.toLowerCase().split(" ").includes(queryString.toLowerCase().trim())
+      )
+    }
+    renderSuggestions(autocompleteResults)
+  }
+  document.getElementById('searchBar').onkeyup = document.getElementById('searchBar').onclick 
+
+
+  function executeSearch(queryString) {
+    let hasFilter = false
+    active_filters.forEach((filter, index) => {
+      if(filter.value == queryString){
+        hasFilter = true
+      }
+    })
+
+    if(hasFilter) return;
+    if((current_category == "No Domain" || current_category == "Misc" || current_category == "Specific Technologies") && queryString == ""){
+      active_filters.forEach((filter, index, array) => {
+        if(topics.includes(filter.value))
+          array.splice(index, 1)
+      })
+    }
+    else if(queryString == ""){
+      active_filters.forEach((filter, index, array) => {
+        if(topics.includes(filter.value))
+          array.splice(index, 1)
+      })
+      active_filters.push({
+        value: current_category,
+      })
+    } else {
+      active_filters.push({
+        value: queryString,
+      })
+    }
+    document.querySelector("#searchBar").value = "";
+    renderFilters();
+    renderResults();
+  }
+
+  function deleteFilter(e){
+    let value = e.path[1].getAttribute("data-value")
+    active_filters.forEach((filter, index) => {
+      if(filter.value == value){
+        active_filters.splice(index, 1)
+      }
+    })
+    // console.log(active_filters)
+    renderFilters();
+    renderResults();
+  }
+
+  function renderFilters() {
+    document.querySelector(".activeFilters").innerHTML = ""
+
+    active_filters.forEach(filter => {
+      // console.log(filter.value)
+      if(filter.value == "" || topics.includes(filter.value)) return;
+      document.querySelector(".activeFilters").innerHTML += `
+        <div class="searchPill pr-2" data-value='${filter.value}'>
+          ${filter.value} 
+          <img class="closeIcon" src="assets/images/x_icon.svg" />
+        </div>`;
+    })
+    Array.from(document.getElementsByClassName("closeIcon")).forEach(elem => {
+      elem.onclick = (e) => {deleteFilter(e)};
+    })
+  }
+
+  function renderResults() {
+    let noResults = true
+    Array.from(document.getElementsByClassName("projectContainer")).forEach(elem => {
+      let deleted = false;
+      active_filters.forEach(filter => {
+        if(deleted) return; 
+
+        if(
+          !projects[elem.getAttribute("data-id")].domains.includes(filter.value) &&
+          !projects[elem.getAttribute("data-id")].tags.includes(filter.value)
+        ){
+          deleted = true
+        }
+
+      })
+      if(deleted) elem.style.display = "none"
+      else {
+        elem.style.display = "block";
+        noResults = false;
+      }
+    })
+    if(noResults) document.querySelector(".galleryError").style.display = "block"
+    else document.querySelector(".galleryError").style.display = "none"
+  }
+
+  function renderSuggestions(items) {
+    let target = document.querySelector(".searchSuggestions");
+    target.innerHTML = "";
+
+    if(items.length == 0){
+      target.innerHTML = "No Results Found";
+    }
+    items.forEach(item => {
+      target.innerHTML += `<div class='searchPill'>${item}</div>`;
+    })
+    Array.from(document.querySelectorAll(".searchSuggestions > .searchPill")).forEach(elem => {
+      elem.onclick = () => {executeSearch(elem.innerHTML)};
+    })
+  }
+
+});
